d9e05d000c0395985e335cbebb41edee8f624bc0
VictorYoloSwagg <victor.pramann@gmail.com>
2022-05-12 08:32:25 +0200

    Add files via upload

--- IAClient.py
+++ IAClient.py
@@ -1,27 +1,12 @@
 import socket
 from jsonStream import readJSON, writeJSON
-from Strategy import strategyf, strategys
 
 
 # On crée un socket client qui va tenter de se connecter au seveur (modèle sensé être statique, il doit donc être en dehors de la class)
-
-
 def client_s():
     s_client = socket.socket()
     s_client.connect(('localhost', 3000))
     return s_client
-
-
-#Pour envoyer le move qu'on veut faire
-
-
-def move(move_played, ia):
-    my_move = {
-        "response": "move",
-        "move": move_played,
-        "message": "Fun message"
-    }
-    writeJSON(ia, my_move)
 
 
 class IAClient:
@@ -32,14 +17,11 @@ class IAClient:
         self._board = None
 
     #Pour lancer le programme
-
     def run(self):
         self.subscribe()
 
     
-     #Pour se connecter au serveur.
-
-
+    #Pour se connecter au serveur.
     def subscribe(self):
         ia_socket = client_s()
         try:
@@ -50,8 +32,8 @@ class IAClient:
                 "port": self._port,
                 "matricules": ["195300", "195242"]
             }
-            readJSON(ia_socket, registration)
-            message = writeJSON(ia_socket)
+            writeJSON(ia_socket, registration)
+            message = readJSON(ia_socket)
             if message["response"] == "ok":
                 self.listen(self._host)
                 self.accept()
@@ -62,16 +44,14 @@ class IAClient:
             print('Connexion avec le serveur non établie')
 
     #Pour écouter sur le certain port défini dans le constructeur
-    
     def listen(self, host):
         self._s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._s.bind(('localhost', self._port))
         self._s.listen(5)
-        '''backlog of size 5'''
+        #backlog of size 5
         print('Listening on port {}'.format(self._port))
 
     #Pour attendre que les clients se connectent et les accepter
-
     def accept(self):
         print('Waiting for client to connect...')
         while True:
@@ -84,28 +64,35 @@ class IAClient:
             except OSError:
                 pass
 
+    #Pour les requêtes 
+    def request(self, ia, message):
+        if message["request"] == "ping":
+            writeJSON(ia, {"response": "pong"})
+
+        if message["request"] == "play":
+            print(message["request"])
+            state = message['state']
+            board = state['board']
+            current = state['current']
+            if current == 0:
+                self.move(ia, board)
+            else:
+                self.move(ia, board, False)
 
     #Pour commencer à jouer et récupérer l'état du jeu, on appelle la class IAStrategy
-
-    def play(self, ia, message):
-        
-        #On commence par récupérer les informations envoyée sur l'état du jeu
-    
-        state = message["state"]
-        cases = message["cases"]
-        current = message["current"]
-        board = state["board"]
-        print(board)
-        print(cases)
-        if current == 0:
-            strategyf(board ,state)
-            print('first')
-        else:
-            strategys(board ,state)
-            print('second')
+    def play(self, to_move, ia):
+        my_move = {
+            "response": "move",
+            "move": to_move,
+            "message": "A joué"
+        }
+        writeJSON(ia, my_move)
 
     def give_up(self):
-        sendJSON(self._s,{"response": "giveup",})
+        writeJSON(self._s,{"response": "giveup",})
+
+    def move(self, ia, board):
+        pass
 
 
 if __name__ == "__main__":
