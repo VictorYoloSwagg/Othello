7f66dc33a929f5a90eae11edaea6cf3e89ab1b1c
VictorYoloSwagg <victor.pramann@gmail.com>
2022-05-16 08:07:31 +0200

    Update IAClient.py

--- IAClient.py
+++ IAClient.py
@@ -1,154 +1,8 @@
+from base64 import encode
 import socket
 import json
-import random
+from Strategy import possibleMoves, myMove
 
-#on récupère ce dont on a besoin du championship runner
-directions = [
-    ( 0,  1),
-    ( 0, -1),
-    ( 1,  0),
-    (-1,  0),
-    ( 1,  1),
-    (-1,  1),
-    ( 1, -1),
-    (-1, -1)
-]
-
-def add(p1, p2):
-    l1, c1 = p1
-    l2, c2 = p2
-    return l1 + l2, c1 + c2
-
-def coord(index):
-    return index // 8, index % 8
-
-def index(coord):
-    l, c = coord
-    return l*8+c
-
-def isInside(coord):
-    l, c = coord
-    return 0 <= l < 8 and 0 <= c < 8
-
-def walk(start, direction):
-    current = start
-    while isInside(current):
-        current = add(current, direction)
-        yield current
-
-def isGameOver(state):
-    playerIndex = state['current']
-    otherIndex = (playerIndex+1)%2
-
-    res = False
-    if len(possibleMoves(state)) == 0:
-        state['current'] = otherIndex
-        if  len(possibleMoves(state)) == 0:
-            res = True
-    state['current'] = playerIndex
-    return res
-
-def willBeTaken(state, move):
-    playerIndex = state['current']
-    otherIndex = (playerIndex+1)%2
-
-    if not (0 <= move < 64):
-        raise BadMove('Your must be between 0 inclusive and 64 exclusive')
-
-    if move in state['board'][0] + state['board'][1]:
-        raise BadMove('This case is not free')
-
-    board = []
-    for i in range(2):
-        board.append(set((coord(index) for index in state['board'][i])))
-
-    move = coord(move)
-
-    cases = set()
-    for direction in directions:
-        mayBe = set()
-        for case in walk(move, direction):
-            if case in board[otherIndex]:
-                mayBe.add(case)
-            elif case in board[playerIndex]:
-                cases |= mayBe
-                break
-            else:
-                break
-
-    if len(cases) == 0:
-        raise BadMove('Your move must take opponent\'s pieces')
-    
-    return [index(case) for case in cases]
-
-def possibleMoves(state):
-    res = []
-    for move in range(64):
-        try:
-            willBeTaken(state, move)
-            res.append(move)
-        except BadMove:
-            pass
-    return res
-
-class GameEnd(Exception):
-	def __init__(self, lastState):
-		self.__state = lastState
-
-	@property
-	def state(self):
-		return self.__state
-
-	def __str__(self):
-		return 'Game Over'
-
-class GameWin(GameEnd):
-	def __init__(self, winner, lastState):
-		super().__init__(lastState)
-		self.__winner = winner
-
-	@property
-	def winner(self):
-		return self.__winner
-
-	def __str__(self):
-		return super().__str__() + ': {} win the game'.format(self.winner)
-
-class BadMove(Exception):
-	pass
-
-class GameDraw(GameEnd):
-	def __init__(self, lastState):
-		super().__init__(lastState)
-
-	def __str__(self):
-		return super().__str__() + ': Draw'
-
-class GameLoop(GameDraw):
-	def __init__(self, lastState):
-		super().__init__(lastState)
-
-	def __str__(self):
-		return super().__str__() + ': Stopped because of lopping behavior'
-
-class BadGameInit(Exception):
-	pass
-
-def CMove(res):                        #choix du move
-    if res!=[]:
-        choix = random.choice(res)     #choisit un move aléatoire parmis ceux disponible
-        return choix
-
-def myMove(choix):
-    try :
-        res = possibleMoves(choix)
-        return {
-                    "response": "move",
-                    "move": CMove(res),
-                    "message": "Let's go"
-                }
-    except Exception :
-                    print("Error")
 
 ipserver="localhost"
 port=3000
@@ -177,8 +31,26 @@ def server():                                                      #fonction qui
             message=json.loads(jeu.recv(2048).decode())
             if message==ping:
                 jeu.send(json.dumps(pong).encode())
-                
+            elif message["request"] == "play":
+                Etat = message["state"]
+                Board = Etat["board"]
+                print(Board)
+                pmove = possibleMoves(Etat)
+                print(pmove)
+                MonMove = {}
+                if len(pmove) == 0:
+                   pass       
+                else:
+                    MonMove = myMove(Etat)
+                    datas = json.dumps(MonMove)
+                    jeu.send(bytes(datas, encoding="utf-8"))    
+                    print("A joué")
+            elif len(message) == 0:
+                s.close()
+            else:
+                print(message)
 
-print("c'est parti !")                      
+
+print("c'est parti !")                     
 if __name__ == "__main__":
     inscription()
